921ac6a30af5dfe914d6515713e165137ef0b537
denis.yakushev <denis.yakushev@burgerkingrus.ru>
2023-04-13 22:23:50 +0300

    iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,59 @@
 package main
 
-func main() {}
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func mainPage(res http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		body, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			panic(err)
+		}
+		tFile, err := ioutil.TempFile("", "")
+		if err != nil {
+			panic(err)
+		}
+		err = ioutil.WriteFile(tFile.Name(), body, 0644)
+		if err != nil {
+			panic(err)
+		}
+		res.WriteHeader(http.StatusCreated)
+		res.Write([]byte("http://localhost:8080/" + filepath.Base(tFile.Name())))
+		// res.Write([]byte("http://" + string(req.URL.Host) + ":" + string(req.URL.Port()) + "/" + filepath.Base(tFile.Name())))
+	} else {
+		tFile, err := ioutil.TempFile("", "")
+		if err != nil {
+			panic(err)
+		}
+		filename := filepath.Dir(tFile.Name()) + req.RequestURI
+		if _, err := os.Stat(filename); err == nil {
+			data, fErr := ioutil.ReadFile(filename)
+			if fErr != nil {
+				panic(fErr)
+			}
+			res.Header().Add("Location", string(data))
+			res.WriteHeader(http.StatusMovedPermanently)
+		} else if errors.Is(err, os.ErrNotExist) {
+			res.WriteHeader(http.StatusNotFound)
+		} else {
+			panic(err)
+		}
+		// res.Write([]byte("Not post: " + filename))
+		defer os.Remove(tFile.Name())
+	}
+}
+
+func main() {
+
+	http.HandleFunc(`/`, mainPage)
+
+	err := http.ListenAndServe(`:8080`, nil)
+	if err != nil {
+		panic(err)
+	}
+}
