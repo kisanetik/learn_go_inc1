99bb0d98ac7c18f3ddb4d2d18c334f6fe8b6c806
denis.yakushev <denis.yakushev@burgerkingrus.ru>
2023-05-10 18:13:01 +0300

    last fix

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
+	"github.com/kisanetik/learn_go_inc1/config"
 	"github.com/kisanetik/learn_go_inc1/internal/handlers"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+	flag.Parse()
+	_, port := config.LoadConfig()
+
 	r := chi.NewRouter()
 
 	r.Get("/{id}", handlers.MethodGet)
 	r.Post("/", handlers.MethodPost)
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(port, r))
 }
