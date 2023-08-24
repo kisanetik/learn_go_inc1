e22a707884d035ba022d0fe9961affbe8fa3ac51
denis.yakushev <denis.yakushev@burgerkingrus.ru>
2023-08-24 16:31:20 +0300

    fix

--- cmd/shortener/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/jackc/pgx/v5/pgxpool
-
-go 1.20
--- internal/storage/database/postgres/postgres.go
+++ internal/storage/database/postgres/postgres.go
@@ -2,8 +2,9 @@ package postgres
 
 import (
 	"context"
-	"github.com/kisanetik/learn_go_inc1/internal/logger"
+
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/kisanetik/learn_go_inc1/internal/logger"
 )
 
 type DB struct {
@@ -18,7 +19,7 @@ func NewDB(addrConn string) (*DB, error) {
 
 	conn, err := pgxpool.NewWithConfig(context.Background(), addr)
 	if err != nil {
-		logger``.Errorf("Error create NewWithConfig : %s", err)
+		logger.Errorf("Error create NewWithConfig : %s", err)
 	}
 
 	db := &DB{
